23b8008a2ff876376a2492517bc9de9ceb53e433
shivajeekhande1 <shivajeekhande1@users.noreply.github.com>
2019-11-10 18:50:43 -0500

    Final commit HW10

--- HW09_Venkata_Khande.py
+++ HW09_Venkata_Khande.py
@@ -2,10 +2,6 @@
 Written by : Venkata Khande
 '''
 
-
-
-
-
 import os
 from collections import defaultdict
 from prettytable import PrettyTable
@@ -25,7 +21,7 @@ class Student():
 
     def prettyTableStudent(self):
         """return the data for this student instance to the pretty table"""
-        return [self.CWID, self.Name, sorted(self.CoursesGradeInformation.keys())]
+        return [self.CWID, self.Name, self.Major, self.CoursesGradeInformation]
 
 class Instructor():
     """Instructor class with details of the Intructor and add course with no of students and pretty table function"""
@@ -43,54 +39,90 @@ class Instructor():
         """Return data of this instructor to the pretty table"""
         return [self.CWID, self.Name, self.Dept], self.Courses
 
+class Majors():
+    """Major class with details of the majors and their required and elective courses."""
+
+    def __init__(self, major):
+        self.major = major
+        self.req = set()
+        self.elec = set()
+
+    def add_required(self, course):
+        self.req = self.req.union(set(course))
+
+    def add_elective(self, course):
+        self.elec = self.elec.union(set(course))
+
 class Pack:
     def __init__(self, FilePath):
 
         self.FilePath = FilePath
         self.studentInfo ={}
         self.insructorsInfo = {}
+        self.majorsData = {}
         self.analyzeFiles()
         self.studentsSummary()
         self.instructor_summary()
+        self.major_summary()
 
     def analyzeFiles(self):
         """Analyse the files for getting the student, Instrctor, and grades data from the text file."""
 
         if not os.path.exists(self.FilePath):
             error_msg = f"File at {self.FilePath} Not Found"
-            raise FileNotFoundError(error_msg)
+            print(error_msg)
 
         studentsFile = 'students.txt'
         instructorsFile = 'instructors.txt'
         gradesFile = 'grades.txt'
+        majorFile = 'majors.txt'
 
         pathJoinStudent = os.path.join(self.FilePath, studentsFile)
         pathJoinInstructor = os.path.join(self.FilePath, instructorsFile)
         filePathGrades = os.path.join(self.FilePath, gradesFile)
+        majorJoinFile = os.path.join(self.FilePath, majorFile)
         
         if studentsFile in os.listdir(self.FilePath):
             
-            for CWID, Name, Major in self.file_reading_gen(pathJoinStudent, 3, "\t", False): 
+            for CWID, Name, Major in self.file_reading_gen(pathJoinStudent, 3, ";", True): 
                 self.studentInfo[CWID]=Student(CWID, Name, Major)
         else:
-            raise FileNotFoundError(f"Can not open {studentsFile}")
+            print(f"Can not open {studentsFile}")
         
         if instructorsFile in os.listdir(self.FilePath):
             
-            for CWID, Name, Dept in self.file_reading_gen(pathJoinInstructor, 3, "\t", False): 
+            for CWID, Name, Dept in self.file_reading_gen(pathJoinInstructor, 3, "|", True): 
                 self.insructorsInfo[CWID]=Instructor(CWID, Name, Dept)
         else:
-            raise FileNotFoundError(f"Can not open {instructorsFile}")
+            print(f"Can not open {instructorsFile}")
 
         if gradesFile in os.listdir(self.FilePath):
             
-            for studentCWID, course, grade, instructorCWID in  self.file_reading_gen(filePathGrades, 4, "\t", False):
+            for studentCWID, course, grade, instructorCWID in  self.file_reading_gen(filePathGrades, 4, "|", True):
                 if studentCWID in self.studentInfo.keys():
                     self.studentInfo[studentCWID].addCourseGrade(course, grade)
                 if instructorCWID in self.insructorsInfo.keys():
                     self.insructorsInfo[instructorCWID].add_course_noofstudents(course) 
         else:
-            raise FileNotFoundError(f"Can't open {gradesFile}")
+            print(f"Can't open {gradesFile}")
+
+        if majorFile in os.listdir(self.FilePath):
+            tmp_dic = defaultdict(lambda: defaultdict(set))
+            for dept, flag, course in self.file_reading_gen(majorJoinFile, 3, "\t", True):
+                tmp_dic[dept][flag].add(course)
+
+            for dept, courses in tmp_dic.items():
+                print(dept)
+                new_major = Majors(dept)
+                
+                for flag, course in courses.items():
+                    if flag == 'R':
+                        new_major.add_required(course)
+                    if flag == 'E':
+                        new_major.add_elective(course)
+                self.majorsData[dept] = new_major
+        else:
+            print(f"Can't open {majorFile}")
 
     def file_reading_gen(self, path, fields, sep=',', header=False):
         """File Reading Function"""
@@ -98,15 +130,14 @@ class Pack:
             File_Path = open(path, "r")
 
         except FileNotFoundError:
-            raise FileNotFoundError(f"{path} Not Found")
+            print(f"{path} Not Found")
 
         else:
             with File_Path:
                 lineNum = 0
                 if header and len(next(File_Path).split(sep)) != fields:
                     File_Path.seek(0)
-                    raise ValueError(
-                        f"{path} has has different fields than expected {fields}")
+                    print(f"{path} has has different fields than expected {fields}")
 
                 if header:
                     lineNum += 1
@@ -115,22 +146,34 @@ class Pack:
                     ln = ln.strip().split(sep)
                     lineNum += 1
                     if len(ln) != fields:
-                        raise ValueError(
+                        print(
                             f"'{path}' has wrong fields on line {lineNum} but expected {fields}")
                     yield tuple(ln)
 
     def studentsSummary(self):
         """Printing pretty table for the student"""
-        pretty = PrettyTable(field_Names = ['CWID', 'Name', 'Completed Courses'])
+        PassGrades = ['A','A-','B+','B','B-','C+','C']
+        pretty = PrettyTable(field_names = ['CWID', 'Name', 'Major','Completed Courses','Remaining Required','Remaining Electives'])
         print(f"Students")
         for student in self.studentInfo.values():
-            pretty.add_row(student.prettyTableStudent())
-
+            test = student.prettyTableStudent()
+            completedCourses = set([Course for Course, grade in test[3].items() if grade in PassGrades])
+            test[3] = sorted(completedCourses)
+            remain_require = self.majorsData[test[2]].req.difference(test[3])
+            if len(self.majorsData[test[2]].elec.intersection(test[3]))>0:
+                remain_elective = None
+            else:
+                remain_elective = self.majorsData[test[2]].elec - completedCourses
+            test.append(remain_require)
+            test.append(remain_elective)
+            pretty.add_row(test)
+            
+        
         print(pretty)
 
     def instructor_summary(self):
         """Printing pretty table for the instructor"""
-        Pretty = PrettyTable(field_Names = ['CWID', 'Name', 'Dept', 'Course', 'Students'])
+        Pretty = PrettyTable(field_names = ['CWID', 'Name', 'Dept', 'Course', 'Students'])
         print(f"Instructors")
         for instructor in self.insructorsInfo.values():
             instructorInformation, Courses = instructor.pretty_table_instructor()
@@ -140,6 +183,13 @@ class Pack:
                 instructorInformation = instructorInformation[0:3]
             
         print(Pretty)
+
+    def major_summary(self):
+        
+        table = PrettyTable(field_names = ['Dept', 'Required', 'Elective'])
+        for dept, major in self.majorsData.items():
+            table.add_row([dept, major.req, major.elec])
+        print(table)
 
 def main():
     try:
